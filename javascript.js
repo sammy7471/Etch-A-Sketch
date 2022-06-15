c4779db286c06d105eb1e04bf1cb8357f2eaaa36
Samuel Solomon <sammygideon01@gmail.com>
2022-06-15 15:16:05 +0000

    Create a Grid of Square Divs

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// javascript
+
+const container = document.querySelector("#container");
+
+function createGrid(num) {  
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement("div");
+        square.classList.add("box");
+        container.appendChild(square);
+    }
+}
+
+createGrid(16);
