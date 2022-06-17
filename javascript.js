4aa1b4e405c8605592a0866ea3c3003f35bd38a3
Samuel Solomon <sammygideon01@gmail.com>
2022-06-17 13:53:15 +0000

    Limit the User Input to a Maximum of 100

--- javascript.js
+++ javascript.js
@@ -17,13 +17,22 @@ function createGrid(num) {
 }
 createGrid(16);
 
+//An addEventListener to a button with a createGrid callback which generates a prompt for a userinput,
+//and creates a Grid acoording to the User's input.
 const button = document.querySelector("#button");
 button.addEventListener("click", () => {
     const input = parseInt(prompt("New Grid Input"), 10);
     document.getElementById("container").innerText = "";
     createGrid(input);
+    // Limits input from 0 to 100
+    if (input < 0 || input > 100) {
+        alert("Value should be between 0 - 100");
+        document.getElementById("container").innerText = "";
+        return;
+    }
 });
 
+//Function that generates a random color
 function generateRandomColor () {
     let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     return randomColor;
